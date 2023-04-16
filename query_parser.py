e1dbbec1bece0c048c390160ffec32935ba43f96
Ming1Xuan <102127451+Ming1Xuan@users.noreply.github.com>
2023-04-16 20:50:03 +0800

    Removed more redundant stuff

--- query_parser.py
+++ query_parser.py
@@ -6,163 +6,6 @@ def parse_sql_query(input):
 
     sql_query = input.lower()
 
-    # Open the file and read its contents
-    #with open(file_path, 'r') as file:
-    #    sql_query = file.read()
-
-    #sql_query = "select returnflag, linestatus, sum(linequantity) as sumqty, count(*) as count_order from table where blah1 and blah4 and blah5 group by blah 2 order by blah 3"
-
-    #sql_query = "select blah1, blah2 from blah3, blah4 where blah5 and blah6 and blah7 group by blah8, blah9 having blah10 order by blah11 limit blah12"
-
-
-    # sql_query = "select\
-    #     blah1,\
-    #     blah2\
-    # from\
-    #     blah3,\
-    #     blah4\
-    # where\
-    #     blah5\
-    #     and blah6\
-    #     and blah7\
-    # group by\
-    #     blah8,\
-    #     blah9\
-    # having\
-    #     blah10\
-    # order by\
-    #     blah11\
-    # limit\
-    #     blah12\
-    # "
-
-
-    # 1.sql-------------------------------------------------------------------------------------------------------------------------------------
-    # sql_query = "select\
-    #     l_returnflag,\
-    #     l_linestatus,\
-    #     sum(l_quantity) as sum_qty,\
-    #     sum(l_extendedprice) as sum_base_price,\
-    #     sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,\
-    #     sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,\
-    #     avg(l_quantity) as avg_qty,\
-    #     avg(l_extendedprice) as avg_price,\
-    #     avg(l_discount) as avg_disc,\
-    #     count(*) as count_order\
-    # from\
-    #     lineitem\
-    # where\
-    #     l_shipdate <= date '1998-12-01' - interval ':1' day\
-    # group by\
-    #     l_returnflag,\
-    #     l_linestatus\
-    # order by\
-    #     l_returnflag,\
-    #     l_linestatus;\
-    # "
-
-
-    # 10.sql-------------------------------------------------------------------------------------------------------------------------------------
-    # sql_query = "select\
-    #     c_custkey,\
-    #     c_name,\
-    #     sum(l_extendedprice * (1 - l_discount)) as revenue,\
-    #     c_acctbal,\
-    #     n_name,\
-    #     c_address,\
-    #     c_phone,\
-    #     c_comment\
-    # from\
-    #     customer,\
-    #     orders,\
-    #     lineitem,\
-    #     nation\
-    # where\
-    #     c_custkey = o_custkey\
-    #     and l_orderkey = o_orderkey\
-    #     and o_orderdate >= date ':1'\
-    #     and o_orderdate < date ':1' + interval '3' month\
-    #     and l_returnflag = 'R'\
-    #     and c_nationkey = n_nationkey\
-    # group by\
-    #     c_custkey,\
-    #     c_name,\
-    #     c_acctbal,\
-    #     c_phone,\
-    #     n_name,\
-    #     c_address,\
-    #     c_comment\
-    # order by\
-    #     revenue desc;\
-    # "
-
-
-    # fake20.sql-------------------------------------------------------------------------------------------------------------------------------------
-    # sql_query = "select\
-    #     s_name,\
-    #     s_address\
-    # from\
-    #     supplier,\
-    #     nation\
-    # where\
-    #     s_suppkey in (\
-    #         select\
-    #             ps_suppkey\
-    #         from\
-    #             partsupp\
-    #         where\
-    #             blah1 = blah2 \
-    #     )\
-    #     and s_nationkey = n_nationkey\
-    #     and n_name = ':3'\
-    # group by\
-    #     s_name\
-    # order by\
-    #     lastoneee;\
-    # "
-
-
-    # 20.sql-------------------------------------------------------------------------------------------------------------------------------------
-    # sql_query = "select\
-    #     s_name,\
-    #     s_address\
-    # from\
-    #     supplier,\
-    #     nation\
-    # where\
-    #     s_suppkey in (\
-    #         select\
-    #             ps_suppkey\
-    #         from\
-    #             partsupp\
-    #         where\
-    #             ps_partkey in (\
-    #                 select\
-    #                     p_partkey\
-    #                 from\
-    #                     part\
-    #                 where\
-    #                     p_name like ':1%'\
-    #             )\
-    #             and ps_availqty > (\
-    #                 select\
-    #                     0.5 * sum(l_quantity)\
-    #                 from\
-    #                     lineitem\
-    #                 where\
-    #                     l_partkey = ps_partkey\
-    #                     and l_suppkey = ps_suppkey\
-    #                     and l_shipdate >= date ':2'\
-    #                     and l_shipdate < date ':2' + interval '1' year\
-    #             )\
-    #     )\
-    #     and s_nationkey = n_nationkey\
-    #     and n_name = ':3'\
-    # order by\
-    #     s_name;\
-    # "
-
-
     # Define regular expressions to match each clause
     select_pattern = re.compile(r'select (.+?) from', re.DOTALL)
     from_pattern = re.compile(r'from (.+?) (?:where|group by|having|order by|limit|$)', re.DOTALL)
@@ -242,7 +85,6 @@ def parse_sql_query(input):
 
         dict['LIMIT'] = limit_clause
 
-
     return dict 
 
 
@@ -251,8 +93,7 @@ def query_compare(query1, query2):
     query_1_dict = parse_sql_query(query1)
     query_2_dict = parse_sql_query(query2)
 
-
-    # Just for testing purposes, simpler to seee
+    # Just for testing purposes, simpler to see
     query_1_dict = {
         'SELECT': ['column1', 'column2', 'column3'],
         'FROM': ['table1', 'table2', 'table3'],
@@ -268,14 +109,6 @@ def query_compare(query1, query2):
         'ORDER BY': ['column1 DESC', 'column3 ASC'],
         'LIMIT': ['100']
     }
-
-
-    print('Final Output:')
-    print('Dictionary for Query 1 is: \n{}'.format(query_1_dict))
-    print("\n")
-    print('Dictionary for Query 2 is: \n{}'.format(query_2_dict))
-    print("\n")
-
 
     for key in ['SELECT', 'FROM', 'WHERE', 'GROUP BY', 'HAVING', 'ORDER BY', 'LIMIT']:
         # Case 1: key exists in both queries
@@ -317,25 +150,7 @@ def query_compare(query1, query2):
             # Case 2.2: key present in query2 but not query1
             if key in query_2_dict and key not in query_1_dict:
                 print(f"\nThe {key} clause was not present in Query 1 but is now present in Query 2")
-        
-        # # Case 2: keys not present in one of the two
-        # elif key in query_1_dict and key not in query_2_dict:
-        #     print(f"The {key} clause was present in Query 1 but is now not present in Query 2")
-        # elif key in query_2_dict and key not in query_1_dict:
-        #     print(f"The {key} clause was not present in Query 1 but is now present in Query 2")
-        # # Case 3: key just not in both queries
-        # else:
-        #     pass
-    
-"""
-(1st specific change in qep) because of 1st specific change in query. The predicates for the select clause have changed. Was present in Query 1 but not Query 2: column2.
 
-(2nd specific change in qep) because of 2nd specific change in query. The predicates for the where clause have changed. Was present in Query 1 but not Query 2: column1 > 10, column2 = "example"
-Was not present in Query 1 but is now present in Query 2: column1 > 5. 
-
-biggest problem: what changes in qep are mapped to the changes in query?
-
-"""
 
 if __name__ == "__main__":
 
@@ -395,7 +210,6 @@ if __name__ == "__main__":
     ORDER BY\
         revenue desc;\
     "
-    
 
     query_compare(query1, query2)
 
